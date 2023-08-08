e036819d5d3048f29e254c99d1a34d88b692f93f
parci8126 <parci8126>
2023-08-08 18:31:40 +0000

    test
    test

---
+++ test notebook.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+print('hello prashant')
+
+# COMMAND ----------
+
+dbutils.fs.ls(/)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select 2*3 as dummy
+
+# COMMAND ----------
+
+_sqldf.show()
+
+# COMMAND ----------
+
+
